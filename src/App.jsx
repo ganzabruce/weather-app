fdc5f8fa23e153344a5edc94977839164856940f
ganzabruce <ganzabruce10.com>
2025-08-27 17:56:33 +0200

    added a search button

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,32 @@ import Footer from './components/footer';
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
+  const [info , setInfo] = useState("")
   console.log(weather)
   const search = async (e) => {
       if(e.key === 'Enter') {
           const data = await fetchWeather(query);
           setWeather(data);
           setQuery('');
+          setInfo("")
       }
   }
+    const getWeather = async () =>{
+        if(query === ""){
+            setInfo("please search for a city")
+        }
+        const data = await fetchWeather(query);
+          setWeather(data);
+          setQuery('');
+          setInfo("")
+    }
   return (
   <div className="main-container">
-        <input type="text" className="search" placeholder="Search a City Name" value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
+        <div className="search-box">
+            <input type="text" className="search" placeholder="Search a City Name" value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
+            <button className="my-btn" onClick={getWeather}><i className='fas fa-search'></i></button>
+        </div>
+        {info && <div>{info}</div>}
         {weather.location && (
             <div className="city">
                 <h2 className="city-name">
